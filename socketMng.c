21c04bcb6433aee1e64ab88ae4dc7ea7a3debe95
Kumark95 <thelordkumark@gmail.com>
2015-11-24 16:20:06 +0100

    Update socketMng.c
    Corregido lo del createServerSocket y acceptNewConnections

--- socketMng.c
+++ socketMng.c
@@ -7,18 +7,16 @@
 #include <stdlib.h>
 #include <errno.h>
 
-#define BACKLOG 1  /* addr to connect */
+#define BACKLOG 1 /* Num conexionx */
 
-// Create a socket and initialize it to be able to accept 
+// Create a socket and initialize it to be able to accept
 // connections.
-// It returns the virtual device associated to the socket that should be used 
-// in the accept system call, to get the virtual device associated to 
+// It returns the virtual device associated to the socket that should be used
+// in the accept system call, to get the virtual device associated to
 // the connection
 //
-
 int createServerSocket (int port)
 {
-
 	/* File descriptor */
 	int fdSock;
 	
@@ -31,7 +29,7 @@ int createServerSocket (int port)
 	/* Actualiza la estructura */
 	struct sockaddr_in server;
 	server.sin_family = AF_INET;
-	server.sin_port = 0; //Comprovar
+	server.sin_port = htons(port); 
 	server.sin_addr.s_addr = INADDR_ANY;
 	bzero(&(server.sin_zero),8);
 
@@ -40,79 +38,72 @@ int createServerSocket (int port)
 		perror("bind error");
 		exit(EXIT_FAILURE);
 	}
-	
-	/* listen */ 
-	if ( (listen(fdSock, BACKLOG)) < 0 ) {
-	perror("listen error");
-	exit(EXIT_FAILURE);	
-	}
-	
-	int sin_size = sizeof(struct sockaddr_in);
-	
-	return accept(fdSock,NULL,NULL);
-}
 
+	/* listen */
+	if ( (listen(fdSock, BACKLOG)) < 0 ) {
+		perror("listen error");
+		exit(EXIT_FAILURE);
+	}
+//printf("Sock: %d ", fdSock);
+	return fdSock;
+}
 
 // Returns the file descriptor associated to the connection.
 // accept system call will fill the socketAddr parameter
 // with the address of the socket for the client which is requesting the
 // connection, and the addrSize parameter with the size of that address.
 
+
 int
 acceptNewConnections (int socket_fd)
 {
 
+	//while(1) {
+	//int sin_size = sizeof(struct sockaddr_in);
+	return accept(socket_fd,NULL,NULL);
 }
 
-// Returns the socket virtual device that the client should use to access 
-// the socket, if the connection is successfully established 
+
+// Returns the socket virtual device that the client should use to access
+// the socket, if the connection is successfully established
 // and -1 in case of error
 //
-// Connect system call requires the address of the 
+// Connect system call requires the address of the
 // server socket to request the connection and the size of that address.
 //
-
 int
 clientConnection (char *host_name, int port)
 {
+	struct sockaddr_in serv_addr;
+	struct hostent * hent;
+	int socket_fd;
+	int ret;
 
-  struct sockaddr_in serv_addr;
-  struct hostent * hent;
-  int socket_fd;
-  int ret;
+	//creates the virtual device for accessing the socket
+	socket_fd = socket (AF_INET, SOCK_STREAM, 0);
+	if (socket_fd < 0)
+		return socket_fd;
 
-  //creates the virtual device for accessing the socket
-  socket_fd = socket (AF_INET, SOCK_STREAM, 0);
-  if (socket_fd < 0)
-    return socket_fd;
-
-  memset((char *) &serv_addr, 0, sizeof(serv_addr));
-
-  serv_addr.sin_family = AF_INET;
-  hent = gethostbyname(host_name);
-  if (hent == NULL) {
-        close (socket_fd);
-	return -1;
-
-  }
-  memcpy((char *)&serv_addr.sin_addr.s_addr, (char *) hent->h_addr, hent->h_length);
-  serv_addr.sin_port = htons(port);
-  serv_addr.sin_family = PF_INET; 
-
-  ret = connect (socket_fd, (struct sockaddr *) &serv_addr, sizeof (serv_addr));
-  if (ret < 0)
-  {
-	  close (socket_fd);
-	  return (ret);
-  } 
-
-  return socket_fd;
-
+	memset((char *) &serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	hent = gethostbyname(host_name);
+	if (hent == NULL) {
+		close (socket_fd);
+		return -1;
+	}
+	memcpy((char *)&serv_addr.sin_addr.s_addr, (char *) hent->h_addr, hent->h_length);
+	serv_addr.sin_port = htons(port);
+	serv_addr.sin_family = PF_INET;
+	ret = connect (socket_fd, (struct sockaddr *) &serv_addr, sizeof (serv_addr));
+	if (ret < 0)
+	{
+		close (socket_fd);
+		return (ret);
+	}
+	return socket_fd;
 }
-
 
 int deleteSocket (int socket_fd)
 {
-  close (socket_fd);
+close (socket_fd);
 }
-
